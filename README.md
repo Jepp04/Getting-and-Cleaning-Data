942323ca3ac8ccacdee88bc8185b18d6998506d9
Ulrich <vanstaden.ulrich@columbus.co.za>
2021-07-15 16:02:26 +0200

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Getting-and-Cleaning-Data
+## Getting-and-Cleaning-Data
+This document is used as a reference to guide you through the repository and it's content. There are 4 main files: 
+
+* README.md
+* CodeBook.Rmd
+* run_analysis.R
+* tidy_Data.txt
+
+### README.md
+This file is the current file you are reading and acts as a guide around this project.
+
+### CodeBook.Rmd
+This document explains all the code and data used within this project in more detail.
+
+### run_analysis.R
+This file contains the actual code to be executed to obtain the final tidy data set as requested by the project. The following steps listed below also listed in the instructions of the projects are used to generate the final data set:
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+###  tidy_Data.txt
+This file contains the final tidy data set with the average over each variable for each activity and each subject.
--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,12 @@
 ## Pre downloaded and extracted data from a Zip file 
-## Read all test data in to R
-activityLabels <- fread("UCI HAR Dataset/activity_labels.txt",sep = " ")
-features <- fread("UCI HAR Dataset/features.txt",sep = " ")
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip  
 
+## Load Required Libraries
+library(dplyr)
+library(tidyr)
+library(data.table)
+
+## Read all test and train data in to R
 # Test Data
 xTest <- fread("UCI HAR Dataset/test/X_test.txt",sep = " ")
 yTest <- fread("UCI HAR Dataset/test/Y_test.txt",sep = " ")
@@ -12,21 +16,36 @@ subjectTest <- fread("UCI HAR Dataset/test/subject_test.txt",sep = " ")
 xTrain <- fread("UCI HAR Dataset/train/X_train.txt",sep = " ")
 yTrain <- fread("UCI HAR Dataset/train/Y_train.txt",sep = " ")
 subjectTrain <- fread("UCI HAR Dataset/train/subject_train.txt",sep = " ")
-# bodyAccX <- fread("UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt",sep = " ")
-# bodyAccY <- fread("UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt",sep = " ")
-# bodyAccZ <- fread("UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt",sep = " ")
-# bodyGyroX <- fread("UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt",sep = " ")
-# bodyGyroY <- fread("UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt",sep = " ")
-# bodyGyroZ <- fread("UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt",sep = " ")
-# TotX <- fread("UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt",sep = " ")
-# TotY <- fread("UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt",sep = " ")
-# TotZ <- fread("UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt",sep = " ")
 
-## Merge data into one Dataset
-# names(xTest) <- features$V2
+## Load all Activity and Feature labels
+activityLabels <- fread("UCI HAR Dataset/activity_labels.txt",sep = " ")
+features <- fread("UCI HAR Dataset/features.txt",sep = " ")
+
+## Add Variable Names
 resultX <- rbind.data.frame(xTest,xTrain)
+names(resultX) <- features$V2
 resultY <- rbind.data.frame(yTest,yTrain)
+names(resultY) <-"Activity"
 resultsubject <- rbind.data.frame(subjectTest,subjectTrain)
-result <- cbind.data.frame(resultsubject,resultY,resultYresultX)
-result <- tbl_df(result)
+names(resultsubject) <-"Subject"
 
+## Replace Activity Number with descriptive labels
+for(i in 1:6)
+{
+  resultY$Activity<-sub(i,activityLabels$V2[i],resultY$Activity)
+}
+
+## Merge data into one Data set
+tidyDataSet <- cbind.data.frame(resultsubject,resultY,resultX)
+
+
+
+## Extract measurements on mean and standard deviation.
+measurementsData <- tidyDataSet[,1:2]
+measurementsData <- cbind.data.frame(measurementsData,tidyDataSet[,grep("mean()|std()",names(tidyDataSet))])
+measurementsData <- tbl_df(measurementsData)
+
+## Tidy data set with the average of each variable for each activity and each subject
+GroupedData <- group_by(measurementsData, Activity, Subject)
+GroupedData <- summarise_each(GroupedData, funs(mean))
+write.table(GroupedData,"tidy_Data.txt",row.names = FALSE)
